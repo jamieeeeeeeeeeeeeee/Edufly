3d329c30e35f387227c26543569107925c924897
jamie <jamie.tan.2023@scis.smu.edu.sg>
2024-11-07 20:40:11 +0800

    i pray everything works sia

--- frontend/Login Page/login.js
+++ frontend/Login Page/login.js
@@ -1,25 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBkep4kR1KlxAHUQM8-dJo-b0cOjUA1tCc",
-  authDomain: "edufly-61bfe.firebaseapp.com",
-  projectId: "edufly-61bfe",
-  storageBucket: "edufly-61bfe.firebasestorage.app",
-  messagingSenderId: "467191151194",
-  appId: "1:467191151194:web:cac30fd47ebff5a7233663",
-  measurementId: "G-NQN411353D"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+import { auth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from '../js/database.js';
 
 // when user manually keys in 
 
@@ -48,7 +27,7 @@ submit.addEventListener("click", function (event) {
 })
 
 // when user uses Google Sign-in method
-const auth = getAuth(app);
+// const auth = getAuth(app);
 auth.languageCode = "en"
 const provider = new GoogleAuthProvider();
 
--- frontend/js/viewHomework.js
+++ frontend/js/viewHomework.js
@@ -1,22 +1,4 @@
-import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
-
-// Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBkep4kR1KlxAHUQM8-dJo-b0cOjUA1tCc",
-    authDomain: "edufly-61bfe.firebaseapp.com",
-    projectId: "edufly-61bfe",
-    storageBucket: "edufly-61bfe.appspot.com",
-    messagingSenderId: "467191151194",
-    appId: "1:467191151194:web:cac30fd47ebff5a7233663",
-    measurementId: "G-NQN411353D"
-};
-
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
-const db = getFirestore(app);
-const auth = getAuth(app);
+import { auth, db, onAuthStateChanged, getFirestore, doc, getDoc, collection, getDocs, setDoc, addDoc, deleteDoc } from '../js/database.js';
 
 // Create Vue application
 const vueApp = Vue.createApp({
@@ -48,10 +30,10 @@ const vueApp = Vue.createApp({
         });
     },
     methods: {
-        // logSelectedClass() {
-        //     console.log(this.classes);
-        //     console.log(this.selectedClass); // Log the selected class to the console
-        // },
+        logSelectedClass() {
+            console.log(this.classes);
+            console.log(this.selectedClass); // Log the selected class to the console
+        },
         async loadHomework() {
             try {
                 const response = await fetch('https://test-mongo-in6ge6b0w-jamies-projects-ac80ffa6.vercel.app/api/homeworks', {
@@ -88,7 +70,7 @@ const vueApp = Vue.createApp({
     },
     watch: {
         selectedClass(newClass) {
-            // this.logSelectedClass(); // Log the selected class whenever it changes
+            this.logSelectedClass(); // Log the selected class whenever it changes
             this.loadHomework(); // Call loadHomework when selectedClass changes
         }
     }
